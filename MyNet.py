5b2b8ef3c631114fb1a5381e4d09675d8f42b6c2
s9bhakta <s9bhakta@ucsd.edu>
2021-06-09 16:30:14 -0700

    edit to mynet

--- MyNet.py
+++ MyNet.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-
+import sys
 
 # class MyNet(nn.Module):
 #     def __init__(self, num_classes=5):
@@ -79,29 +79,51 @@ class MyNet(nn.Module):
             nn.Dropout(),
             nn.MaxPool2d(kernel_size=2),
             nn.ReLU(),
+#             nn.LeakyReLU(inplace=True),
             
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
             nn.MaxPool2d(kernel_size=2),
             nn.ReLU(),
+#             nn.LeakyReLU(inplace=True),
             
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.Dropout(),
             nn.MaxPool2d(kernel_size=2),
             nn.ReLU(),
+#             nn.LeakyReLU(inplace=True),
             
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
+#             nn.Dropout(),
             nn.MaxPool2d(kernel_size=2),
             nn.ReLU(),
+#             nn.LeakyReLU(inplace=True),
             
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+#             nn.Conv2d(64, 64, kernel_size=3, padding=1),
+#             nn.MaxPool2d(kernel_size=2),
+#             nn.ReLU(),
+            
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.MaxPool2d(kernel_size=2),
             nn.ReLU(),
+#             nn.LeakyReLU(inplace=True),
+            
+            nn.Conv2d(128, 256, (3,3), padding = 1),
+            nn.BatchNorm2d(256),
+            nn.MaxPool2d(kernel_size=2),
+            nn.ReLU(), #[12, 2304]
+#             nn.LeakyReLU(inplace=True),
+            
+#             nn.Conv2d(256,512, (3,3), padding = 1),
+#             nn.BatchNorm2d(512), #[12, 512]
+#             nn.MaxPool2d(kernel_size=2),
+#             nn.ReLU(), #[12, 2304]
             
         )
         
         self.classifier = nn.Sequential(
-            nn.Linear(7 * 7 * 64, 2048),
+            
+            nn.Linear(3*3*256, 2048), # batch size x 
             nn.Dropout(),
             nn.LeakyReLU(),
             nn.Linear(2048, 1024),
@@ -114,6 +136,8 @@ class MyNet(nn.Module):
 
         x = self.layers(x)
         x = x.view(x.size(0), -1)
+#         print(x.shape)
+#         sys.exit()
         x = self.classifier(x)
         scores = x
         
